237847cbe34ec3a703815f6eff98b774fc2312d1
CallumDoty <dotycf@vcu.edu>
2022-11-29 18:31:12 -0500

    final

---
+++ scripts.js
@@ -0,0 +1,22 @@
+function green() {
+    $("body").css("background-color", "green");
+
+}
+
+function blue() {
+    $("body").css("background-color", "blue");
+
+}
+
+
+function red() {
+    $(".screen").removeClass("active");
+    $(".red").addClass("active");
+}
+
+
+function orange() {
+    $(".screen").removeClass("active");
+    $(".orange").addClass("active");
+}
+
